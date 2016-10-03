d1b13bd3609a17cbdc19d0a6d5d2f14fe0e614ed
derilevi <derilevi@gmail.com>
2016-10-04 01:19:14 +0200

    run on specified port

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=os.getenv('PORT'))
